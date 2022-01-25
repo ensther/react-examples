74896db19deeec3b8624423948f2890e747b5bbe
Esther <ensther@gmail.com>
2022-01-25 16:23:06 +0100

    Added useState to change value automatically of some field

--- 01-starting-setup/src/components/ExpenseItem/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,25 @@
 import './ExpenseItem.scss';
 
+import React, {useState} from 'react';
 import Card from '../Card/Card';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title); //To update title on change automatically
+
+    const clickHandler = (event) => {
+        if(title.indexOf(', updated!') === -1)
+            setTitle(title + ', updated!');
+    }
+
     return (
         <Card id={'expese-item__' + props.id} className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <p className='expense-item__price'>{props.amount}$</p>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
