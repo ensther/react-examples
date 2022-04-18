9ebbac7ab423174bd947157b0f89631771557ae4
Esther <ensther@gmail.com>
2022-04-18 11:05:47 +0200

    Lesson 12

---
+++ 12-starting-project/src/App.js
@@ -0,0 +1,24 @@
+import React, {useCallback, useState} from 'react';
+
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
+
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toogleParagraph = useCallback(() => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toogleParagraph}>{showParagraph ? 'Hide' : 'Show'} paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
